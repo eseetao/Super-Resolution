86aa94166580e57e3c1f23900de557f21ee63bc7
MathewSam <m1sam@eng.ucsd.edu>
2022-03-02 18:15:21 -0800

    Adding Loss function for content loss

---
+++ Core/Loss.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class ContentLoss(nn.Module):
+    """Constructs a content loss function based on the VGG19 network.
+    Using high-level feature mapping layers from the latter layers will focus more on the texture content of the image.
+    Paper reference list:
+        -`Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network <https://arxiv.org/pdf/1609.04802.pdf>` paper.
+        -`ESRGAN: Enhanced Super-Resolution Generative Adversarial Networks                    <https://arxiv.org/pdf/1809.00219.pdf>` paper.
+        -`Perceptual Extreme Super Resolution Network with Receptive Field Block               <https://arxiv.org/pdf/2005.12597.pdf>` paper.
+     """
+
+    def __init__(self):
+        super(ContentLoss, self).__init__()
+        # Load the VGG19 model trained on the ImageNet dataset.
+        vgg19 = models.vgg19(pretrained=True).eval()
+        # Extract the thirty-sixth layer output in the VGG19 model as the content loss.
+        self.feature_extractor = nn.Sequential(*list(vgg19.features.children())[:36])
+        # Freeze model parameters.
+        for parameters in self.feature_extractor.parameters():
+            parameters.requires_grad = False
+
+        # The preprocessing method of the input data. This is the VGG model preprocessing method of the ImageNet dataset.
+        self.register_buffer("mean", torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer("std", torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+
+    def forward(self, sr, hr):
+        # Standardized operations
+        sr = sr.sub(self.mean).div(self.std)
+        hr = hr.sub(self.mean).div(self.std)
+
+        # Find the feature map difference between the two images
+        loss = F.l1_loss(self.feature_extractor(sr), self.feature_extractor(hr))
+
+        return loss
