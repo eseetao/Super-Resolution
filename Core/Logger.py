1a94751e5f5aaee63d7dfbec75c665671043eaf1
MathewSam <m1sam@eng.ucsd.edu>
2022-03-02 19:33:35 -0800

    Removing Logger for now

--- Core/Logger.py
+++
@@ -1,8 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-
-class Logger:
-    '''
-    Class to handle tensroboard summary writer
-    '''
-    def __init__(self,config):
-        self.writer = SummaryWriter(config.exp_name)
